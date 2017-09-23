2d957cca9103d386c89efe68497b42cf1c7e32c8
Ubuntu <835943102@qq.com>
2017-09-23 22:14:35 +0800

    table

--- JS/popDialog.js
+++ JS/popDialog.js
@@ -1,19 +1,63 @@
 /**
  * Created by lmy on 17-9-14.
  */
-function editStudio(r) {
 
-    let sN = document.getElementById('studioTable').rows[r].cells[0].innerHTML;
-    let sRow = document.getElementById('studioTable').rows[r].cells[1].innerHTML;
-    let sCol = document.getElementById('studioTable').rows[r].cells[2].innerHTML;
-    let sType = document.getElementById('studioTable').rows[r].cells[4].innerHTML;
-    let sStatus = document.getElementById('studioTable').rows[r].cells[5].innerHTML;
-    // alert(sType);
-    $("#sName").val(sN);
-    $("#srow").val(sRow);
-    $("#scol").val(sCol);
+let num = 0;
+
+function getRowNumber() {
+    if (event.srcElement.tagName == 'TD') {
+        let row = event.srcElement.parentElement;
+        num = row.rowIndex;
+    }
+    else num = 0;
+}
+
+function add() {
+    let name = document.getElementById('studioName').value;
+    let row = document.getElementById('row').value;
+    let col = document.getElementById('col').value;
+    let status = document.getElementById('studioStatus').value;
+    let type = document.getElementById('studioType').value;
+
+    let newRow = document.getElementById('studioTable').insertRow();
+
+    let studioName = newRow.insertCell();
+    let seatRow = newRow.insertCell();
+    let seatCell = newRow.insertCell();
+    let totalNumber = newRow.insertCell();
+    let studioType = newRow.insertCell();
+    let studioStatus = newRow.insertCell();
+
+    studioName.innerText = name;
+    seatRow.innerText = row;
+    seatCell.innerText = col;
+    studioStatus.innerText = status;
+    totalNumber.innerText = row * col;
+    studioType.innerText = type;
+}
+
+function editStudio() {
+
+    let lenght = document.getElementById('studioTable').rows[num].cells.length;
+
+    let arr = [];
+    for (let i = 0; i < lenght; i++) {
+        arr[i] = document.getElementById('studioTable').rows[num].cells[i].innerHTML;
+    }
+
+    $("#sName").val(arr[0]);
+    $("#srow").val(arr[1]);
+    $("#scol").val(arr[2]);
     // $("#select").find("option[value=sType]").attr("selected", true);
+}
 
+function del() {
+    let row = num;
+    if (row == 0)
+        return 0;
+    else {
+        document.getElementById('studioTable').deleteRow(row);
+    }
 }
 
 $(function () {
--- JS/seatStatus.js
+++ JS/seatStatus.js
@@ -15,8 +15,6 @@ $(document).ready(function () {
 
             'cccccccccccccc',
 
-            '______________',
-
             'cccccccccccccc',
 
             'cccccccccccccc',
